a826d4e5a1d254c8833ebae854f8d5493e9e754e
Katerina <cakeinpanic@gmail.com>
2015-04-10 22:19:01 +0300

    Add stylus processing to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,16 @@
 var gulp = require('gulp');
+var stylus = require('gulp-stylus');
 
-gulp.task('default', ['jsx'], function() {
+gulp.task('default', ['jsx', 'stylus'], function() {
 	gulp.watch('src/**/*.jsx', ['jsx']);
+    gulp.watch('src/styl/**/*', ['stylus']);
 });
 
+gulp.task('stylus', function () {
+    gulp.src('./src/styl/floodIt.styl')
+        .pipe(stylus())
+        .pipe(gulp.dest('./dist/css/'));
+});
 
 gulp.task('jsx', function() {
 	"use strict";
